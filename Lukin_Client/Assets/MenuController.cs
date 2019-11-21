bda00328d8eb3b193ea3cb1d2e9cc37e25912d58
Allan <allan.chen@maxfortune.tw>
2019-11-21 14:51:10 +0800

    背包

---
+++ Lukin_Client/Assets/MenuController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] catclaw;
+    private bool click = false;
+
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    public void menuclick()
+    {
+        click = !click;
+        foreach (var claw in catclaw)
+        {
+            claw.GetComponent<MenuButtonDotween>().Buttontouch(click);
+        }
+    }
+}
---
+++ Lukin_Client/Assets/Script/ScrollbarReset.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollbarReset : MonoBehaviour
+{
+    public float ScrollbarStartPos = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Scrollbar>().value = ScrollbarStartPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
